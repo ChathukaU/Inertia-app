1d3156c1eaf3564c9dd7c984f5979624381f0b1f
Chathuka Upamith <wnacuweerasingha@students.nsbm.ac.lk>
2024-03-29 18:00:30 +0530

    1

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,7 +13,32 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                
+                <table class="table-auto">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Intro to CSS</td>
+                            <td>Adam</td>
+                            <td>8585</td>
+                        </tr>
+                        <tr class="bg-emerald-200">
+                            <td>A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design</td>
+                            <td>Adam</td>
+                            <td>8585</td>
+                        </tr>
+                        <tr>
+                            <td>Intro to JavaScript</td>
+                            <td>Chris</td>
+                            <td>8585</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </AuthenticatedLayout>
